a2fcbf917a45bf0507d7638d7fb0b8677c3bc82d
Bharathimula <bharathim273@gmail.com>
2025-07-19 08:35:44 +0530

    modified lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,12 +3,9 @@ resource "aws_lambda_function" "processor" {
   runtime       = "python3.11"
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "lambda_function.lambda_handler"
-  filename      = "lambda_code/data_processor.py"
-
-  source_code_hash = filebase64sha256("lambda_code/data_processor.py")
   filename      = "lambda_code/data_processor.zip"
-
   source_code_hash = filebase64sha256("lambda_code/data_processor.zip")
+
   environment {
     variables = {
       DB_USER = "admin"
