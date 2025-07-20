676baf4c5811cdce278f97e9791d6c4238d04158
Bharathimula <bharathim273@gmail.com>
2025-07-20 09:07:50 +0530

    modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,13 @@
 module "networking" {
   source = "./networking"
 
-  aws_region           = var.aws_region
-  vpc_id               = var.vpc_id
-  private_subnet_ids   = var.private_subnet_ids
-  db_user              = var.db_user
-  db_pass              = var.db_pass
-  db_security_group_id = var.db_security_group_id
-  domain_name          = var.domain_name
+  project_name          = var.project_name
+  aws_region            = var.aws_region
+  vpc_id                = var.vpc_id
+  private_subnet_ids    = var.private_subnet_ids
+  db_user               = var.db_user
+  db_pass               = var.db_pass
+  db_security_group_id  = var.db_security_group_id
+  domain_name           = var.domain_name
 }
 
