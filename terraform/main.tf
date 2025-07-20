a06f3f6ba52b93c03d9349c490f092915685dc0d
Bharathimula <bharathim273@gmail.com>
2025-07-20 09:06:08 +0530

    modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,12 @@
 module "networking" {
-  source       = "./networking"
-  project_name = var.project_name
+  source = "./networking"
+
+  aws_region           = var.aws_region
+  vpc_id               = var.vpc_id
+  private_subnet_ids   = var.private_subnet_ids
+  db_user              = var.db_user
+  db_pass              = var.db_pass
+  db_security_group_id = var.db_security_group_id
+  domain_name          = var.domain_name
 }
 
