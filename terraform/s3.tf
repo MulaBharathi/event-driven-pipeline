43e90dd08d521c718432ac4688162e64a745453b
Bharathimula <bharathim273@gmail.com>
2025-07-21 16:59:41 +0530

    Modified

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,4 +2,25 @@ resource "aws_s3_bucket" "data_bucket" {
   bucket = "${var.project_name}-data-bucket"
   force_destroy = true
 }
+# Allow S3 to invoke Lambda
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.processor.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.data_bucket.arn
+}
+
+# Trigger Lambda when a new CSV is uploaded to S3
+resource "aws_s3_bucket_notification" "lambda_trigger" {
+  bucket = aws_s3_bucket.data_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.processor.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".csv"
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3]
+}
 
