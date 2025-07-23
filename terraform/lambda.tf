a4c0ddaef2996c20592c4185b1d09261bee495d3
Bharathimula <bharathim273@gmail.com>
2025-07-23 17:25:23 +0530

    modified

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -35,4 +35,22 @@ resource "aws_lambda_function" "report_generator" {
   timeout     = 10
   memory_size = 128
 }
+resource "aws_lambda_permission" "allow_s3_invoke" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.processor.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.data_bucket.arn  # replace with your S3 bucket resource name
+}
+
+resource "aws_s3_bucket_notification" "notify_lambda" {
+  bucket = aws_s3_bucket.data_bucket.id  # replace with your S3 bucket resource name
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.processor.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3_invoke]
+}
 
