dc093c704740b83d4b21c77be57790708b47f989
Bharathimula <bharathim273@gmail.com>
2025-07-23 17:16:36 +0530

    modified

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,18 +1,41 @@
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "${var.project_name}-lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambda_logs" {
+  name       = "${var.project_name}-lambda-logs"
+  roles      = [aws_iam_role.lambda_exec_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_lambda_function" "processor" {
   function_name = "${var.project_name}-processor"
   runtime       = "python3.11"
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "data_processor.lambda_handler"
 
-  s3_bucket         = "event-driven-pipeline-data-bucket"
-  s3_key            = "lambda/data_processor.zip"   
-  source_code_hash  = filebase64sha256("../lambda/data_processor.zip")
+  filename         = "${path.module}/../lambda/data_processor.zip"
+  source_code_hash = filebase64sha256("${path.module}/../lambda/data_processor.zip")
 
   environment {
     variables = {
-      DB_USER = "admin"
+      DB_USER = var.db_user
     }
   }
+
+  timeout     = 10
+  memory_size = 128
 }
 
 resource "aws_lambda_function" "report_generator" {
@@ -21,14 +44,16 @@ resource "aws_lambda_function" "report_generator" {
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "report_generator.lambda_handler"
 
-  s3_bucket         = "event-driven-pipeline-data-bucket"
-  s3_key            = "lambda/report_generator.zip"     # ✅ fixed this line
-  source_code_hash  = filebase64sha256("../lambda/report_generator.zip")
+  filename         = "${path.module}/../lambda/report_generator.zip"
+  source_code_hash = filebase64sha256("${path.module}/../lambda/report_generator.zip")
 
   environment {
     variables = {
-      DB_USER = "admin"
+      DB_USER = var.db_user
     }
   }
+
+  timeout     = 10
+  memory_size = 128
 }
 
