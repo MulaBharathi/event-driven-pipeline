c8df093ee5c501953a97b182110c615311b801f0
Bharathimula <bharathim273@gmail.com>
2025-07-23 17:19:05 +0530

    modified

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,24 +1,3 @@
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "${var.project_name}-lambda-exec-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_policy_attachment" "lambda_logs" {
-  name       = "${var.project_name}-lambda-logs"
-  roles      = [aws_iam_role.lambda_exec_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
 resource "aws_lambda_function" "processor" {
   function_name = "${var.project_name}-processor"
   runtime       = "python3.11"
