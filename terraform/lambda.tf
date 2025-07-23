4119967dac8311dd27455137b72718df5bd23f84
Bharathimula <bharathim273@gmail.com>
2025-07-23 17:42:55 +0530

    modified

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,8 +1,8 @@
-# Lambda function: data processor
+# Lambda Function: Data Processor
 resource "aws_lambda_function" "processor" {
   function_name = "${var.project_name}-processor"
   runtime       = "python3.11"
-  role          = aws_iam_role.lambda_exec_role.arn   # reference IAM role from iam.tf
+  role          = aws_iam_role.lambda_exec_role.arn
   handler       = "data_processor.lambda_handler"
 
   filename         = "${path.module}/../lambda/data_processor.zip"
@@ -18,11 +18,11 @@ resource "aws_lambda_function" "processor" {
   memory_size = 128
 }
 
-# Lambda function: report generator
+# Lambda Function: Report Generator
 resource "aws_lambda_function" "report_generator" {
   function_name = "${var.project_name}-report-generator"
   runtime       = "python3.11"
-  role          = aws_iam_role.lambda_exec_role.arn   # reference IAM role from iam.tf
+  role          = aws_iam_role.lambda_exec_role.arn
   handler       = "report_generator.lambda_handler"
 
   filename         = "${path.module}/../lambda/report_generator.zip"
@@ -38,18 +38,18 @@ resource "aws_lambda_function" "report_generator" {
   memory_size = 128
 }
 
-# Allow S3 to invoke processor Lambda
+# Permission for S3 to invoke the processor Lambda
 resource "aws_lambda_permission" "allow_s3_invoke" {
   statement_id  = "AllowS3Invoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.processor.function_name
   principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.data_bucket.arn
+  source_arn    = aws_s3_bucket.data_bucket.arn  # your S3 bucket resource name here
 }
 
-# S3 bucket notification to trigger processor Lambda on new object creation
+# S3 Bucket notification to trigger processor Lambda on object created
 resource "aws_s3_bucket_notification" "notify_lambda" {
-  bucket = aws_s3_bucket.data_bucket.id
+  bucket = aws_s3_bucket.data_bucket.id  # your S3 bucket resource name here
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.processor.arn
@@ -59,18 +59,20 @@ resource "aws_s3_bucket_notification" "notify_lambda" {
   depends_on = [aws_lambda_permission.allow_s3_invoke]
 }
 
-# CloudWatch Event Rule to trigger report_generator Lambda daily
+# CloudWatch Event rule to trigger report_generator daily
 resource "aws_cloudwatch_event_rule" "daily_report" {
   name                = "${var.project_name}-daily-report"
   schedule_expression = "rate(1 day)"
 }
 
+# CloudWatch Event target for invoking report_generator Lambda
 resource "aws_cloudwatch_event_target" "invoke_report_generator" {
   rule      = aws_cloudwatch_event_rule.daily_report.name
   target_id = "ReportGenerator"
   arn       = aws_lambda_function.report_generator.arn
 }
 
+# Permission for CloudWatch Events to invoke report_generator Lambda
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_report_generator" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
